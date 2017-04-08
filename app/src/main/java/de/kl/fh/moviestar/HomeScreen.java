2dd87300224b186cfb06b0013ab5168d656ffe69
Marcus Gurschek <Marcus Gurschek>
2017-04-08 09:20:50 +0200

    Erstellen des Repos.

---
+++ app/src/main/java/de/kl/fh/moviestar/HomeScreen.java
@@ -0,0 +1,37 @@
+package de.kl.fh.moviestar;
+
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+
+
+public class HomeScreen extends AppCompatActivity implements View.OnClickListener{
+
+    private Button collect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        //Buttons
+        collect = (Button) findViewById(R.id.collection);
+
+        //Listener
+        collect.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v){
+        if(v == collect){
+            Intent collIntent = new Intent(this, Collection.class);
+            startActivity(collIntent);
+        }
+    }
+}
---
+++ app/src/test/java/de/kl/fh/moviestar/HomeScreenTest.java
@@ -0,0 +1,10 @@
+package de.kl.fh.moviestar;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Marcus on 08.04.2017.
+ */
+public class HomeScreenTest {
+
+}
