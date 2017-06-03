f56a5492d1c45d9319031d84625aa2209bb5f964
Marcus Gurschek <Marcus Gurschek>
2017-06-03 14:52:49 +0200

    +

--- app/src/main/java/de/kl/fh/moviestar/SingleMovie.java
+++ app/src/main/java/de/kl/fh/moviestar/SingleMovie.java
@@ -1,13 +1,19 @@
 package de.kl.fh.moviestar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SingleMovie extends AppCompatActivity {
 
+    String ID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_movie);
+
+        Intent myIntent = getIntent();
+        ID = myIntent.getStringExtra("ID");
     }
 }
