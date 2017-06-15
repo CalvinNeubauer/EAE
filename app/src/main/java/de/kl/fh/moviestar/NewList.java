1e7fc5f3a7c9ea76ac10723d30a50b69782ded09
KALLE\Kalle <Calvin.Neubauer@gmx.de>
2017-06-15 14:13:25 +0200

    Ups. Vergessen die Sachen zu adden

---
+++ app/src/main/java/de/kl/fh/moviestar/NewList.java
@@ -0,0 +1,51 @@
+package de.kl.fh.moviestar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Kalle on 14.06.2017.
+ */
+
+public class NewList extends AppCompatActivity implements View.OnClickListener {
+
+    private Button newListButton;
+    private EditText nameV;
+    private String type,listName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newlist);
+
+        type = getIntent().getStringExtra("type");
+
+        newListButton = (Button) findViewById(R.id.newListButton);
+        newListButton.setOnClickListener(this);
+        nameV = (EditText) findViewById(R.id.nameET);
+    }
+
+    public void onClick(View v){
+        listName = nameV.getText().toString().trim();
+        Intent returnIntent = new Intent();
+        if(!listName.isEmpty()) {
+            DatabaseManager db = DatabaseManager.getInstance(this);
+            if (type.equals("Movies")) {
+                db.addMovieList(listName);
+            } else if (type.equals("Series")) {
+                db.addSeriesList(listName);
+            }
+        }else
+            Toast.makeText(this, "List name can't be empty.",
+                    Toast.LENGTH_LONG).show();
+
+        setResult(RESULT_OK,returnIntent);
+        finish();
+    }
+
+}
